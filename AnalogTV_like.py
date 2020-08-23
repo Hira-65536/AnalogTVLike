217b039c098130e6ecdd315cb2c087ca5b8a0141
hira65536 <retera5198myon@gmail.com>
2020-08-23 19:24:31 +0900

    Ver1.01

--- AnalogTV_like.py
+++ AnalogTV_like.py
@@ -1,12 +1,12 @@
 # coding: utf-8
 import sys
 import numpy as np
-from PIL import Image, ImageFilter,ImageChops,ImageDraw,ImageEnhance
+from PIL import Image, ImageFilter,ImageChops,ImageDraw,ImageEnhance,ImageSequence
 import os,tkinter, tkinter.filedialog, tkinter.messagebox
 
 
 TV_Like = 'n'
-
+GIF_FLAG = False
 cnt=0
 fname = 'setting.txt'
 with open(fname,"r") as file:
@@ -55,39 +55,26 @@ def Color_setting(img):
 
     return img
 
+def get_frames(path):
+    '''パスで指定されたファイルのフレーム一覧を取得する
+    '''
+    im = Image.open(path)
+    return (frame.copy() for frame in ImageSequence.Iterator(im))
+
+
+
 try:
-    def main():
-        # ファイル選択ダイアログの表示
-        root = tkinter.Tk()
-        root.withdraw()
-        fTyp1 = [("", "*.png")]
-        iDir = os.path.abspath(os.path.dirname(__file__))
-        image_data = tkinter.filedialog.askopenfilename(filetypes=fTyp1,initialdir=iDir)
-        print('AnalogTVLike-Converter START')
-        #image_data=input('plz image pass:')
-        if os.path.isfile(image_data):
-            print(image_data)
-            title=image_data[:-4]
-            input_img = Image.open(image_data)
-        else:
-            print('this image not exist.')
-            sys.exit()
-        x,y=input_img.size
-        if x>1920 and y>1080:
-            x=1920
-            y=1080
-            input_img=input_img.resize((x,y))
-            print('resize image')
+    def img_convert(input_img):
         input_img = input_img.convert('RGBA')
         gray_img = input_img.convert('L')
-        #ノイズの追加
-        noise_img=AnalogLike_noise(gray_img)
-        #透明化のためにRGBAに変換
+        # ノイズの追加
+        noise_img = AnalogLike_noise(gray_img)
+        # 透明化のためにRGBAに変換
         new_gray_img = noise_img.convert('RGBA')
-        #グレースケールを透過
+        # グレースケールを透過
         new_gray_img.putalpha(int(TRANSMITTANCE))
-        #合成（乗算）
-        mix_img=ImageChops.multiply(input_img, new_gray_img)
+        # 合成（乗算）
+        mix_img = ImageChops.multiply(input_img, new_gray_img)
 
         Analoglike_img = Color_setting(mix_img)
         Analoglike_img.putalpha(int(TRANSMITTANCE))
@@ -102,22 +89,71 @@ try:
         enhance_image = enhancer.enhance(BRIGHTNESS)
         enhance_image.putalpha(255)
         enhance_image.filter(ImageFilter.GaussianBlur(GAUSSIAN))
-        #TV_Like=input('add frame?[y/N]') or 'n'
-        TV_Like=tkinter.messagebox.askquestion('showquestion', 'フレームを追加しますか？')
-        if TV_Like=='yes':
-            frame = Image.open('sample/frame.png')
-            frame_resize=frame.resize(enhance_image.size)
-            frame_resize = frame_resize.convert('RGBA')
-            enhance_image.paste(frame_resize,(0,0),frame_resize.split()[3])
-            out_frame = ('_frame_')
+        return enhance_image
+
+    def main():
+        count=0
+        result_img=[]
+        # ファイル選択ダイアログの表示
+        root = tkinter.Tk()
+        root.withdraw()
+        fTyp1 = [("", "*.*")]
+        iDir = os.path.abspath(os.path.dirname(__file__))
+        image_data = tkinter.filedialog.askopenfilename(filetypes=fTyp1,initialdir=iDir)
+        print('AnalogTVLike-Converter START')
+        #image_data=input('plz image pass:')
+        format_img=image_data[-3:]
+        if format_img=='png' or format_img=='jpg':
+            GIF_FLAG=False
+        elif format_img=='gif':
+            GIF_FLAG=True
         else:
-            out_frame =('_')
-        result_img = enhance_image
-        result_img.show()
-        if 'yes'==tkinter.messagebox.askquestion('showquestion', 'これで保存しますか？'):
-            result_img.save(title+out_frame+'output.png')
+            tkinter.messagebox.showerror('確認', '画像ファイルを選択してください。')
+            sys.exit()
+        if os.path.isfile(image_data) and os.path.isfile('sample/frame.png'):
+            print(image_data)
+            title=image_data[:-4]
+            frames = get_frames(image_data)
+        else:
+            print('this image not exist.')
+            tkinter.messagebox.showerror('確認', '加工画像またはフレームがみつかりません。\nフレーム画像（frame.png）は、sampleという名前のディレクトリに入れてください。')
+            sys.exit()
+        TV_Like = tkinter.messagebox.askquestion('showquestion', 'フレームを追加しますか？')
+        for input_img in frames:
+            x,y=input_img.size
+            if x>1920 and y>1080:
+                x=1920
+                y=1080
+                input_img=input_img.resize((x,y))
+                print('resize image')
+            enhance_image=img_convert(input_img)
+            #TV_Like=input('add frame?[y/N]') or 'n'
+            if TV_Like=='yes':
+                frame = Image.open('sample/frame.png')
+                frame_resize=frame.resize(enhance_image.size)
+                frame_resize = frame_resize.convert('RGBA')
+                enhance_image.paste(frame_resize,(0,0),frame_resize.split()[3])
+                out_frame = ('_frame_')
+            else:
+                out_frame =('_')
+            result_img.append(enhance_image)
+            count+=1
+            print(str(count)+'枚目の処理終了')
+
+        #result_img.show()
+        if GIF_FLAG:
+            tkinter.messagebox.showinfo('確認', 'GIFアニメの確認画面はないため、保存後にご自身でご確認お願いします。')
+        else:
+            result_img[0].show()
+        if 'yes'==tkinter.messagebox.askquestion('showquestion', '保存しますか？'):
+            if GIF_FLAG:
+                result_img[0].save(title+out_frame+'output.gif',save_all=True, append_images=result_img[1:],duration=100,loop=0)
+            else:
+                result_img[0].save(title + out_frame + 'output.' + format_img)
+            tkinter.messagebox.showinfo('確認','保存に成功しました。')
             print('output success!!')
         else:
+            tkinter.messagebox.showerror('確認', '保存に失敗しました。')
             print('output failed!!')
 
     if __name__ == '__main__':
